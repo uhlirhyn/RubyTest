fc50c08fea18eb984c327983ac4bf102a7677ad7
Hynek Uhlíř <uhlirhyn@fit.cvut.cz>
2011-10-11 20:56:35 +0200

    ukol 3

--- freq.rb
+++ freq.rb
@@ -1,42 +1,42 @@
 def sameword(file)
 
-				s = ""
-				e = {}
+	s = ""
+	e = {}
 
-				f = File.open(file,"r") 
-				f.readlines.each do
-								|r|
-								for i in 0..(r.size-1)
-												if (r[i] == ' ') || (i == r.size-1)
+	f = File.open(file,"r") 
+	f.readlines.each do
+		|r|
+		for i in 0..(r.size-1)
+			if (r[i] == ' ') || (i == r.size-1)
 
-																if s != ""
-																				# print "s=\"#{s}\"\n"
-																				s = s.downcase
-																				if e[s] == nil 
-																								e[s] = 1
-																				else
-																								e[s] += 1
-																				end
-																				s = ""
-																end
-												else
-																s += r[i]
-												end			
-								end
+				if s != ""
+					# print "s=\"#{s}\"\n"
+					s = s.downcase
+					if e[s] == nil 
+						e[s] = 1
+					else
+						e[s] += 1
+					end
+					s = ""
 				end
+			else
+				s += r[i]
+			end			
+		end
+	end
 
-				e.each do
-								|k,v| 
+	e.each do
+		|k,v| 
 
-								print "#{k} #{v}\n"
-				end
+		print "#{k} #{v}\n"
+	end
 
-				f.close
+	f.close
 
 end
 
 if ARGV[0]==nil
-				print "Nebyl zadan nazev souboru\n"
+	print "Nebyl zadan nazev souboru\n"
 else 
-				sameword(ARGV[0])
+	sameword(ARGV[0])
 end 
