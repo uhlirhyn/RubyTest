25493549975689a907d56c0660ef92e3318b8e70
Hynek Uhlíř <uhlirhyn@fit.cvut.cz>
2011-10-31 18:42:37 +0100

    score fixed

--- tennis_scorer/lib/tennis_scorer.rb
+++ tennis_scorer/lib/tennis_scorer.rb
@@ -1,23 +1,20 @@
 class TennisScorer
 
 	def initialize
-		@score = "0-0"
+		@rec = 0
+		@ser = 0
 	end
 
-	attr_reader :score
+	def score
+		"#{@ser == 45 ? 40 : @ser}-#{@rec == 45 ? 40 : @rec}"
+	end
 
 	def rec_p
-		@score = case @score
-				 when "0-0" then "0-15"
-				 when "15-0" then "15-15"
-				 end
+		@rec = (@rec + 15) % 60
 	end
 
 	def ser_p
-		@score = case @score
-				 when "0-0" then "15-0"
-				 when "0-15" then "15-15"
-				 end
+		@ser = (@ser + 15) % 60
 	end
 
 end
