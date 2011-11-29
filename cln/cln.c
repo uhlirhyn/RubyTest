edc3923b31036c7e184d38bfc505c76b969176bb
Hynek Uhlíř <uhlirhyn@fit.cvut.cz>
2011-11-29 18:34:31 +0100

    bytecode implemented, ...

--- cln/cln.c
+++ cln/cln.c
@@ -5,11 +5,31 @@
 #include <assert.h>
 #include <stdio.h>
 
+// Bytecode interpret
+
 #define HEAP_SIZE 1024  // 1 KB
 #define STACK_SIZE 1024 // 1 KB
 #define BUFFER 128
 
-struct var * stack_end;
+#define test(t) { assert(t); printf("\e[1m\e[32m|\e[0m"); }
+
+// VM globals
+struct program * pr;    // program
+struct stack * st;      // zasobnik
+struct gc * g;          // pamet (GC)
+
+typedef struct stack {
+    char * start;       // Zacatek stacku
+    char * sp;          // Stack pointer - ukazuje na prazdne pole
+    char * fp;          // Frame pointer - zacatek ramce, ukazuje na prvni pole ramce
+} stack;
+
+typedef struct program {
+    char * filename;        // jmeno souboru
+    char * bytes;           // instrukce (nacteny soubor)
+    int size;               // velikost souboru
+    int ip;                 // aktualni instrukce (index BC) ktera se ma provest
+} program;
 
 // Sum: 16 B
 typedef struct obj {
@@ -55,12 +75,6 @@ typedef struct env {
     struct env * prev;      // predchozi env
 } env;
 
-// funkce
-typedef struct fce {
-    
-
-}
-
 void scavenge(gc * gcl) {
 
     //....
@@ -105,36 +119,93 @@ env * leave_env(env * en) {
 
 }
 
-void init(gc * gcl, env * ge) {
+// http://www.anyexample.com/programming/c/how_to_load_file_into_memory_using_plain_ansi_c_language.xml
+int load_file(const char *filename, char **result) { 
+    
+    int size = 0;
+    FILE * f = fopen(filename, "rb");
+
+    if (f == NULL) { 
+        *result = NULL;
+        return -1;      // -1 means file opening fail 
+    } 
+
+    fseek(f, 0, SEEK_END);
+    size = ftell(f);
+    fseek(f, 0, SEEK_SET);
+    *result = (char *) malloc(size + 1);
+    
+    if (size != fread( *result, sizeof(char), size, f)) { 
+        free(*result);
+        return -2;      // -2 means file reading fail 
+    } 
+    
+    fclose(f);
+    (*result)[size] = 0;
+    return size;
+}
+
+// vynuluje ciselniky apod. - vyresetuje VM
+void reset_vm() {
+
+    //==========
+    //    GC
+    //==========
+
+    // vynuluj pamet
+    bzero((void*) g->mem, g->real_size);
+    bzero((void*) g->old, g->real_size);
+
+    // zalozeni freelistu;
+    g->list = (freelist *) g->mem;
+    g->list->size = g->real_size;
+    g->list->next = NULL;
+
+    //==========
+    //  STACK
+    //==========
+    
+    st->sp = st->start;
+    st->fp = st->start;
+
+    //===========
+    //  PROGRAM
+    //===========
+    
+    pr->ip = 0;
+}
+
+void init() {
 
     //==========
     //    GC
     //==========
 
     // alokace meho pametoveho prostoru
-    gcl->real_size = HEAP_SIZE / 2;
-    gcl->mem = (obj *) malloc(gcl->real_size);
+    g->real_size = HEAP_SIZE / 2;
+    g->mem = (obj *) malloc(g->real_size);
     
     // oldspace
-    gcl->old = (obj *) malloc(gcl->real_size);
-
-    // vynuluj pamet
-    bzero((void*) gcl->mem, gcl->real_size);
-    bzero((void*) gcl->old, gcl->real_size);
-
-    // zalozeni freelistu;
-    gcl->list = (freelist *) gcl->mem;
-    gcl->list->size = gcl->real_size;
-    gcl->list->next = NULL;
+    g->old = (obj *) malloc(g->real_size);
 
     //==========
     //  STACK
     //==========
 
-    ge->vars = (var *) malloc(STACK_SIZE);
-    ge->size = 0;
-    ge->prev = NULL;
-    stack_end = (var *) ((void *) ge->vars + STACK_SIZE);
+    st->start = malloc(STACK_SIZE);
+
+    //===========
+    //  PROGRAM
+    //===========
+    
+    pr->size = load_file(pr->filename, &pr->bytes);
+    if (pr->size < 0) { 
+        puts("\e[31mError loading file\e[0m");
+        abort();
+    } 
+
+    reset_vm();
+
 }
 
 void mem_dump(gc * gcl, int grouping) {
@@ -152,21 +223,6 @@ void mem_dump(gc * gcl, int grouping) {
             printf("\t%X ",*(mem + i++));
             if (i > gcl->real_size/sizeof(char)) break;
         }
-        printf("\n");
-    }
-
-}
-
-void print_envs(env * en) {
-
-    printf("----------------------------------------\n");
-    printf("  ENV LIST \n");
-    printf("----------------------------------------\n");
-
-    printf("Env size: %d\n", en->size);
-
-    for (int i=0; i < en->size; i++) {
-        printf("%s = %p",(en->vars + i)->id,(en->vars + i)->value);
         printf("\n");
     }
 
@@ -266,24 +322,6 @@ char * new_str(gc * gcl, char * str) {
     return s;
 }
 
-int insert_var(env * e, char * id, obj * value, gc * gcl) {
-
-    // na jakou pozici to budu davat
-    var * position = e->vars + e->size;
-
-    // je tam misto ?
-    if (position < stack_end) {
-        position->id = new_str(gcl, id);
-        position->value = value;
-        e->size++;
-    } else {
-        // TODO - stack error (out of stack)
-        return 0;
-    }
-
-    return 1;  
-}
-
 // console mark
 void cm() {
     printf("giraffe > ");
@@ -319,143 +357,330 @@ void store_number(obj * o, int value) {
 
 }
 
+//====================
+// BYTECODE instrukce
+//====================
+
+void st_check(char * address) {
+    
+    if ( address > st->start + STACK_SIZE + 1) {
+        fprintf(stderr, " \e[31mStack overflow\e[39m\n");
+        abort();
+    }
+    
+    if ( address < st->start ) {
+        fprintf(stderr, " \e[31mStack underflow\e[39m\n");
+        abort();
+    }
+}
+
+// push 0x01 
+// vloz 1B hodnotu na zasobnik
+void push(char value) {
+    st_check(st->sp);
+    *(st->sp) = value;      // na adresu SP dej hodnotu
+    st->sp++;
+}
+
+// pop 0x02
+// vydej 1B hodnotu ze zasobniku
+char pop() {
+    st_check(st->sp - 1);
+    st->sp--;
+    return *(st->sp);
+}
+
+// push_i 0x03 
+// vloz 4B hodnotu na zasobnik
+void push_i(int value) {
+    st_check(st->sp + 3);       // +3 protoze na te 4. uz SP je
+    *((int *) st->sp) = value;  // na adresu SP dej hodnotu
+    st->sp += 4;
+}
+
+// pop_i 0x04
+// vydej 4B hodnotu ze zasobniku
+int pop_i() {
+    st_check(st->sp - 4);   // tady musim ale krokovat zpet opravdu o ty 4
+    st->sp -= 4;
+    return *((int *) st->sp);
+}
+
+// push_p 0x07 
+// vloz pointer na zasobnik
+void push_p(char * value) {
+    st_check(st->sp + sizeof(char *) - 1);  // viz push_i
+    *((char **) st->sp) = value;            // na adresu SP dej hodnotu
+    st->sp += sizeof(char *);
+}
+
+// pop_p 0x08
+// vydej pointer ze zasobniku
+char * pop_p() {
+    st_check(st->sp - sizeof(char *));
+    st->sp -= sizeof(char *);
+    return *((char **) st->sp);
+}
+
+// add_char 0x05
+// secti 1B hodnoty
+void add_c() {
+    char op1 = pop();
+    char op2 = pop();
+    push(op1 + op2);
+}
+
+// sub_char 0x06
+// odecti 1B hodnoty
+void sub_c() {
+    char op1 = pop();
+    char op2 = pop();
+    push(op1 - op2);
+}
+
+// call 0x09
+// http://unixwiz.net/techtips/win32-callconv-asm.html
+// call od adresy (parametry jsou jiz ulozene na zasobniku !!!)
+// POZOR -- FP ukazuje na prvni pole noveho ramce, takze pod nim
+// pri volani neni adresa stareho FP - ta je adresu za nim !!! (lepe se to psalo)
+void call(int address) {
+    push_i(pr->ip + 5); // vlozi na zasobnik navratovou adresu 4B adresa callu + 1B opcode "call"
+                        // (+1 protoze ted je ip na instrukci call, ktera prave bezi...)
+    push_p(st->fp);     // vlozi na zasobnik stary fp    
+    st->fp = st->sp;    // nastav novy fp
+    pr->ip = address;   // skoc na telo funkce
+}
+
+// return 0x0a
+void ret() {
+    st->sp = st->fp;    // nastav vrchol zasobniku na zacatek ramce
+    st->fp = pop_p();   // ziskej stary fp
+    pr->ip = pop_i();   // ziskej navratovou adresu
+}
+
+// store locale 1B 0x0b
+// locals jsou cislovane od 0
+void sl_c(char value, unsigned int offset) {
+    st_check(st->fp + offset);
+    *(st->fp + offset) = value;
+}
+
+// load locale 1B 0x0c
+// locals jsou cislovane od 0
+char ll_c(unsigned int offset) {
+    st_check(st->fp + offset);
+    return *(st->fp + offset);
+}
+
+// store in arguments 1B 0x0d
+// argumenty jsou cislovane od 0
+void sa_c(char value, unsigned int offset) {
+    // krok zpet, krok pres stare FP a pres IP (ty jsou ale delsi nez 1B)
+    int stepback = 1 + 2 * sizeof(char *);
+    st_check(st->fp - offset - stepback);  
+    *(st->fp - offset - stepback) = value;
+}
+
+// load from arguments 1B 0x0e
+// argumenty jsou cislovane od 0
+char la_c(unsigned int offset) {
+    // krok zpet, krok pres stare FP a pres IP (ty jsou ale delsi nez 1B)
+    int stepback = 1 + 2 * sizeof(char *);
+    st_check(st->fp - offset - stepback); 
+    return *(st->fp - offset - stepback);
+}
+
+// control output 0x0e
+void out_c() {
+    printf("%x", pop());
+}
+
+//===============
+// VM EXECUTION
+//===============
+
+// vezme dalsi byte z bytecodu
+char next() {
+    return pr->bytes[pr->ip++];
+}
+
+// http://www.cprogramming.com/tutorial/cfileio.html
+// http://www.ibm.com/developerworks/ibm/library/it-haggar_bytecode/
+void run() {
+
+    char opcode;
+    char pc;
+    int pi;
+    char pa[4];
+
+    // skoc na "main"
+    // prvni int v souboru je adresa mainu
+    pa[3] = next();
+    pa[2] = next();
+    pa[1] = next();
+    pa[0] = next();
+    pi = *((int *) pa);
+    pr->ip = pi;
+
+    printf(" Header 4B: \e[33m-- Main found on %d (0x%02x)\e[0m\n", pi, pi);
+
+    // provadej instrukce
+    while (pr->ip < pr->size) {
+
+        opcode = next();
+        printf(" Byte: 0x%02x ", opcode);
+
+        switch (opcode) {
+        case 0x01:
+            printf("\e[36m-- push \e[0m");
+            pc = next();
+            printf("\e[36m%c (0x%02x)\e[0m",pc ,pc);
+            push(pc);
+            break;
+        case 0x0a:
+            printf("\e[36m-- ret \e[0m");
+            ret();
+            break;
+        case 0x02:
+            printf("\e[36m-- pop \e[0m");
+            pop();
+            break;
+        case 0x09:
+            printf("\e[36m-- call \e[0m");
+            pa[3] = next();
+            pa[2] = next();
+            pa[1] = next();
+            pa[0] = next();
+            pi = *((int *) pa);
+            printf("\e[36m%d (0x%02x)\e[0m",pi ,pi);
+            call(pi);
+            break;
+        case 0x03:
+            printf("\e[36m-- push_i \e[0m");
+            pa[3] = next();
+            pa[2] = next();
+            pa[1] = next();
+            pa[0] = next();
+            pi = *((int *) pa);
+            printf("\e[36m%d (0x%02x)\e[0m",pi ,pi);
+            push_i(pi);
+            break;
+        case 0x00:
+            printf("\e[36m-- nop\e[0m");
+            break;
+        case 0x0f:
+            printf("\e[36m-- out_c\e[0m");
+            out_c();
+            break;
+        default:
+            printf("\e[33m-- WARNING: Uknown opcode 0x%02x (ignored)\e[0m", opcode);
+        }
+
+        printf("\n");
+    }
+
+}
+
 int main ( int argc, char **argv ) {
 
     // zaloz GC a alokuj pamet
-    // zaloz stack a na nem global env
+    // zaloz stack
     gc gcl;
-    env ge;
-    init(&gcl, &ge);
+    stack stk;
+    program prg;
+    
+    g = &gcl;
+    st = &stk;
+    pr = &prg;
+
+    // TODO kontrola existence parametru
+    pr->filename = argv[1];
+
+    init();
 
     printf("----------------------------------------\n");
     printf(" Giraffe !\n----------------------------------------\n");
 
-    printf(" Mem location:\t%p - %p\n",gcl.mem, gcl.mem + gcl.real_size);
-    printf(" Real size:\t%d B\n",gcl.real_size);
+    printf(" Mem location:\t%p - %p\n",g->mem, g->mem + g->real_size);
+    printf(" Real size:\t%d B\n",g->real_size);
     printf(" Stack size:\t%d B\n",STACK_SIZE);
 
     printf("----------------------------------------\n");
-    
+
     char str[BUFFER];
     int int_arg;
+
+    // BYTECODE test ... 
+    printf(" Tests: ");
+  
+    // push / pop
+    push(15);
+    test(pop() == 15);
     
-    // aktualni env je ten global
-    env * current_env = &ge;
+    // add_char
+    push(11);
+    push(12);
+    add_c();
+    test(pop() == 23);
 
-    // REPL
-    while (1) {
+    // sub_char
+    push(11);
+    push(12);
+    sub_c();
+    test(pop() == 1);
 
-        // cti instrukci
-        do {
-            cm();
-        } while (read_str(str) == 0);
+    // push_i / pop_i
+    push_i(256);
+    test(pop_i() == 256);
 
-        // alokace
-        if (strcmp(str,"al") == 0) {
-            do {
-                printf("How much allocate? : ");
-            } while (read_number(&int_arg) == 0);
-            obj * alloc_1 = allocate(&gcl, int_arg * sizeof(obj));
-            printf("Allocated %d obj-s (%d bytes) from address %p to %p\n", 
-                    int_arg, int_arg * sizeof(obj), alloc_1, 
-                    (void *) alloc_1 + int_arg * sizeof(obj));
-            do {
-                printf("Store in variable: ");
-            } while (read_str(str) == 0);
-            if (insert_var(current_env, str, alloc_1, &gcl)) {
-                printf("Reference on address %p stored in var %s\n", 
-                        alloc_1, str);
-            } else {
-                printf("Store failed ...\n");
-            }
-            continue;
-        }
+    // push_p / pop_p
+    push_p((char *) g);   
+    test((struct gc *) pop_p() == g);
 
-        // print memory
-        if (strcmp(str,"pm") == 0) {
-            mem_dump(&gcl,20);
-            continue;
-        }
+    // call
+    char * _sp = st->sp;
+    char * _fp = st->fp;
+    pr->ip = 15;
+    call(2);
+    test(pr->ip == 2);
 
-        // print freelist
-        if (strcmp(str,"pf") == 0) {
-            print_freelist(&gcl);
-            continue;
-        }
+    // ret
+    ret();
+    test(pr->ip == 20); // pokracuje dal v puvodnich instrukcich
+    test(st->fp == _fp);
+    test(st->sp == _sp);
+   
+    // locale store / load
+    sl_c(15,2);
+    test(ll_c(2) == 15);
 
-        // quit
-        if (strcmp(str,"q") == 0) {
-            printf("Quit ...\n");
-            break;
-        }
+    // argumenty store / load
+    push(20);   // arg1
+    push(25);   // arg0
+    call(5);
+    test(la_c(0) == 25);
+    test(la_c(1) == 20);
+    sa_c(65,0); // change arg0
+    sa_c(30,1); // change arg1
+    test(la_c(0) == 65);
+    test(la_c(1) == 30);
+    ret();
+    test(pop() == 65);
+    test(pop() == 30);
 
-        // print env
-        if (strcmp(str,"pe") == 0) {
-            print_envs(current_env);
-            continue;
-        }
+    printf("\n");
 
-        // new env
-        if (strcmp(str,"ne") == 0) {
-            current_env = new_env(current_env);
-            continue;
-        }
+    // end of tests
+    
+    printf("----------------------------------------\n");
 
-        // leave env
-        if (strcmp(str,"le") == 0) {
-            current_env = leave_env(current_env);
-            continue;
-        }
+    reset_vm(); // vyresetuj VM po testech
+    run();
 
-        // store 
-        if (strcmp(str,"sn") == 0) {
-            do {
-                printf("Save into variable: ");
-            } while (read_str(str) == 0);
-            obj * o = lookup_var(current_env, str);
-            if (o != NULL) {
-                do {
-                    printf("Enter value: ");
-                } while (read_number(&int_arg) == 0);
-                store_number(o, int_arg);
-            } else {
-                printf("Variable '%s' is not declared\n", str);
-            }
-            continue;
-        }
-
-        // load
-        if (strcmp(str,"l") == 0) {
-            do {
-                printf("Load from variable: ");
-            } while (read_str(str) == 0);
-            obj * o = lookup_var(current_env, str);
-            if (o != NULL) {
-                printf("Variable '%s' = '%p'\n", str, o);
-            } else {
-                printf("Variable '%s' is not declared\n", str);
-            }
-            continue;
-        }
-
-        // help
-        if (strcmp(str,"h") == 0) {
-            printf("pm - print memory\n");
-            printf("pf - print freelist\n");
-            printf("al - alocate\n");
-            printf("q - quit\n");
-            printf("h - help\n");
-            printf("le - leave env\n");
-            printf("ne - new env\n");
-            printf("sn - store number\n");
-            printf("l - load\n");
-            continue;
-        }
-
-        printf("Unknown instruction '%s'\n",str);
-    }
+    printf("----------------------------------------\n");
 
     // uvolni pamet
-    free(gcl.mem);
+    free(g->mem);
+    free(st->start);
 
 }
